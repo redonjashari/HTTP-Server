a151062510d5da16a40ad2700cd8b5d7a0d141f7
Redon <redon.jasharip@gmail.com>
2026-03-03 14:52:38 +0100

    Built handle client connection function

--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+// #include "message_handle_tools.h" to be built soon
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
@@ -15,7 +16,72 @@ struct args {
 
 // Function to handle client connection
 static void *handle_connection(void *arg) {
-    // Logic for handling connection will be implemented in next push
+    struct args *args = (struct args*) arg;
+
+    // create buffer for request
+    char request_buffer[MAX_REQUEST_SIZE];
+    int request_size;
+
+    // receive the request
+    request_size = recv(args->client_socket, request_buffer, MAX_REQUEST_SIZE, 0);
+    if (request_size == -1) {
+        perror("recv");
+        close(args->client_socket);
+        return NULL;
+    }
+    puts("Request received!");
+
+    // parse the request
+    struct message new_request;
+    if (parse_request(&new_request, request_buffer, request_size) == -1) {
+        fprintf(stderr, "parse_request: error");
+        
+        // send bad request message
+        char *bad_request_response = 
+        "HTTP/1.1 400 Bad Request\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 11\r\n"
+        "\r\n\r\n"
+        "Bad Request";
+
+        if (send(args->client_socket, bad_request_response, strlen(bad_request_response), 0) == -1) {
+            perror("send");
+        }
+
+        message_cleanup(&new_request);
+        close(args->client_socket);
+        return NULL;
+    }
+    puts("Request parsed!");
+
+    // create response
+    struct message new_response;
+    if (create_response(&new_request, &new_response, &args->POST_lock) == -1) {
+        fprintf(stderr, "create_response: error");
+        message_cleanup(&new_request);
+        close(args->client_socket);
+        return NULL;
+    }
+    puts("Response created!");
+
+    // 6 is the number of '\r' and '\n' characters that are needed in the response
+    int response_size = strlen(new_response.line) + strlen(new_response.headers) 
+                        + strlen(new_response.body) + 6;
+    char response_buffer[response_size];
+    sprintf(response_buffer, "%s\r\n"
+                                "%s\r\n"
+                                "\r\n"
+                                "%s",
+                                new_response.line, new_response.headers, new_response.body);
+    send(args->client_socket, response_buffer, response_size, 0);
+
+    puts(response_buffer);
+
+    message_cleanup(&new_request);
+    message_cleanup(&new_response);
+    close(args->client_socket);
+
+    return NULL;
 }
 
 int main(void) {
